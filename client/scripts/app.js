0d80700e49f6434d9bd56504aabda4a6c46360ce
ejachang <ajkchang@gmail.com>
2017-12-08 16:38:45 -0800

    some tests passed

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -4,17 +4,40 @@ var app = {
   messages: {}
 };
 app.init = function () {
-  
+ 
 };
 
-app.send = function () {
-  
+
+app.send = function (message) {
+  $.ajax({
+    url: app.server,
+    type: 'POST',
+    data: message,
+    contentType: 'application/json',
+    datatype: 'JSON',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      console.error('chatterbox: Failed to send message', data);
+    }
+  });
 };
 
 app.fetch = function () {
-  
+  $.ajax({
+    url: app.server,
+    type: 'GET',
+    success: function (data) {
+      console.log('chatterbox: Message received');
+    },
+    error: function (data) {
+      console.error('chatterbox: Failed to receive message', data);
+    }
+  });
 };
 app.clearMessages = function () {
+  
   
 };
 app.renderMessage = function () {
@@ -30,36 +53,3 @@ app.handleSubmit = function () {
   
 };
 
-var message = {
-  username: 'shawndrost',
-  text: 'trololo',
-  roomname: '4chan'
-};
-
-$.ajax({
-  // This is the url you should use to communicate with the parse API server.
-  url: 'http://parse.sfs.hackreactor.com/chatterbox/classes/messages',
-  type: 'GET',
-  success: function (data) {
-    console.log('chatterbox: Message received');
-  },
-  error: function (data) {
-    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-    console.error('chatterbox: Failed to receive message', data);
-  }
-});
-
-$.ajax({
-  // This is the url you should use to communicate with the parse API server.
-  url: 'http://parse.sfs.hackreactor.com/chatterbox/classes/messages',
-  type: 'POST',
-  data: JSON.stringify(message),
-  contentType: 'application/json',
-  success: function (data) {
-    console.log('chatterbox: Message sent');
-  },
-  error: function (data) {
-    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-    console.error('chatterbox: Failed to send message', data);
-  }
-});
